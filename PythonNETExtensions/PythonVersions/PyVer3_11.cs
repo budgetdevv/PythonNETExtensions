75bf94766bc1029b6c14a5b02bcd148c9f93bf8e
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-03-15 00:26:49 +0800

    Fix bundle

--- PythonNETExtensions/PythonVersions/PyVer3_11.cs
+++ PythonNETExtensions/PythonVersions/PyVer3_11.cs
@@ -4,7 +4,7 @@ namespace PythonNETExtensions.PythonVersions
 {
     public readonly struct PyVer3_11: IPythonVersion
     {
-        public static string OSXPythonBundleDownloadURL => "https://drive.usercontent.google.com/download?id=1v-ddEOnkNszZGhXmh1UQjCvYfRAfS4Kc&export=download&authuser=1&confirm=t&uuid=d9817ef1-7a5f-41ce-8b9d-1cdc88f07641&at=APZUnTUxuSG3nFARelCdTsSvPfoi%3A1710318036536";
+        public static string OSXPythonBundleDownloadURL => "https://github.com/budgetdevv/PythonNETExtensions/raw/main/Bundles/3.11/OSX/PythonBundle.zip";
         public static string WindowsPythonBundleDownloadURL => throw new NotImplementedException();
         public static string LinuxPythonBundleDownloadURL => throw new NotImplementedException(); 
     }
