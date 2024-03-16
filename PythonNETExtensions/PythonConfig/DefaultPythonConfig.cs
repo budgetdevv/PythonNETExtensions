f8edfee643aed761487b271eaebea0b26daa1f44
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-03-16 17:20:23 +0800

    Path fix

--- PythonNETExtensions/PythonConfig/DefaultPythonConfig.cs
+++ PythonNETExtensions/PythonConfig/DefaultPythonConfig.cs
@@ -2,7 +2,7 @@ namespace PythonNETExtensions.PythonConfig
 {
     public struct DefaultPythonConfig: IPythonConfig<DefaultPythonConfig>
     {
-        public static string PythonBundleContainingDirectory => "./";
+        public static string PythonBundleContainingDirectory => string.Empty;
         public static string PythonBundleDirectoryName => "PythonHome";
     }
 }
