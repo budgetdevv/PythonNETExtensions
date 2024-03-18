facae62be50ce71ebe15d8e33c9f4a630bec18f3
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-03-18 17:40:22 +0800

    Added TextHelpers.cs

---
+++ PythonNETExtensions/Helpers/TextHelpers.cs
@@ -0,0 +1,13 @@
+using System.Runtime.CompilerServices;
+
+namespace PythonNETExtensions.Helpers
+{
+    public static class TextHelpers
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string IndentCode(this string code, string indentation = " ")
+        {
+            return $"{indentation}{code.Replace("\n", $"\n{indentation}")}";
+        }
+    }
+}
