56c0b4eba342f40856f53fb14bf1f85570037eaf
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-03-21 18:11:50 +0800

    Fixed TextHelpers.IndentCode()

--- PythonNETExtensions/Helpers/TextHelpers.cs
+++ PythonNETExtensions/Helpers/TextHelpers.cs
@@ -5,7 +5,7 @@ namespace PythonNETExtensions.Helpers
     public static class TextHelpers
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static string IndentCode(this string code, string indentation = " ")
+        public static string IndentCode(this string code, string indentation = "    ")
         {
             return $"{indentation}{code.Replace("\n", $"\n{indentation}")}";
         }
