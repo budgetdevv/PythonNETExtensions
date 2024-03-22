1897fa3242d7672376b10df13646f4ac6d435957
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-03-23 03:07:33 +0800

    Added InspectModule.cs

---
+++ PythonNETExtensions/Modules/InspectModule.cs
@@ -0,0 +1,7 @@
+namespace PythonNETExtensions.Modules
+{
+    public struct InspectModule: IPythonBuiltInModule<InspectModule>
+    {
+        public static string ModuleName => "inspect";
+    }
+}
